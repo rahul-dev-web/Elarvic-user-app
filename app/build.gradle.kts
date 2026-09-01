535146f975fde035f3042f5346f3c9e793a4f901
Rahul Kumar Jha <jarahul989@gmail.com>
2026-09-01 14:42:04 +0530

    Configure user app for key login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.android.gms:play-services-auth:21.3.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
